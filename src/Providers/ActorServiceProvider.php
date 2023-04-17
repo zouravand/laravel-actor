0879eae85858800dc768ebb866283efa3bd13f81
Pouya Zouravand <pouya.zuravand@gmail.com>
2023-04-17 11:54:03 +0330

    remove additional code from provider

--- src/Providers/ActorServiceProvider.php
+++ src/Providers/ActorServiceProvider.php
@@ -2,10 +2,10 @@
 
 namespace Tedon\LaravelActor\Providers;
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Foundation\Console\AboutCommand;
 use Illuminate\Support\ServiceProvider;
-use Tedon\LaravelActor\Helpers\NamingHelper;
+use Tedon\LaravelActor\Actor;
+use Tedon\LaravelActor\Facades\Actor as ActorFacade;
 
 /**
  * @method actor(string $action, $hasType, $hasTimestamp, $indexName, $shouldIndex)
@@ -20,43 +20,13 @@ class ActorServiceProvider extends ServiceProvider
 
         AboutCommand::add('Laravel Actor', fn() => ['Version' => '0.0.1']);
 
-        $this->defineActorBlueprintMacro();
-        $this->defineCustomBlueprintMacro();
+        ActorFacade::defineMacros();
     }
 
-    private function defineActorBlueprintMacro(): void
+    public function register(): void
     {
-        Blueprint::macro('actor', function (string $action = 'act', $hasType = true, $hasTimestamp = false, $indexName = null, $shouldIndex = false) {
-            /** @var Blueprint $this */
-            $actor = NamingHelper::getActor($action);
-            $acted = NamingHelper::getActed($action);
-
-            $field = $this->unsignedBigInteger("{$actor}_id")->nullable();
-            if ($hasType)
-                $this->string("{$actor}_type")->nullable();
-
-            if ($hasTimestamp)
-                $this->timestamp("{$acted}_at")->nullable();
-
-            if ($shouldIndex) {
-                if ($hasType)
-                    $this->index(["{$actor}_type", "{$actor}_id"], $indexName);
-                else
-                    $this->index(["{$actor}_id"], $indexName);
-            }
-
-            return $field;
-        });
-    }
-
-    private function defineCustomBlueprintMacro(): void
-    {
-        Blueprint::macro('creator', function ($hasType = true, $hasTimestamp = false, $indexName = null, $shouldIndex = false) {
-            return $this->actor('create', $hasType, $hasTimestamp, $indexName, $shouldIndex);
-        });
-
-        Blueprint::macro('editor', function ($hasType = true, $hasTimestamp = false, $indexName = null, $shouldIndex = false) {
-            return $this->actor('edit', $hasType, $hasTimestamp, $indexName, $shouldIndex);
+        $this->app->bind('laravel-actor', function () {
+            return new Actor();
         });
     }
 }
