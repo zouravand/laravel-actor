20631eb65b7b49f7fa53e4af0ba24a6f9b313973
Pouya Zouravand <pouya.zuravand@gmail.com>
2023-05-13 15:32:42 +0330

    add timestamp by default

--- src/Actor.php
+++ src/Actor.php
@@ -18,35 +18,52 @@ class Actor
 
     private function defineBaseMacro(): void
     {
-        Blueprint::macro('actor', function (string $action = 'act', $hasType = true, $hasTimestamp = false, $indexName = null, $shouldIndex = false) {
-            /** @var Blueprint $this */
-            $actor = NamingHelper::getActor($action);
-            $acted = NamingHelper::getActed($action);
+        Blueprint::macro(
+            'actor',
+            function (
+                string $action = 'act',
+                $hasType = true,
+                $hasTimestamp = true,
+                $indexName = null,
+                $shouldIndex = false
+            ) {
+                /** @var Blueprint $this */
+                $actor = NamingHelper::getActor($action);
+                $acted = NamingHelper::getActed($action);
 
-            $field = $this->unsignedBigInteger("{$actor}_id")->nullable();
-            if ($hasType)
-                $this->string("{$actor}_type")->nullable();
+                $field = $this->unsignedBigInteger("{$actor}_id")->nullable();
+                if ($hasType) {
+                    $this->string("{$actor}_type")->nullable();
+                }
 
-            if ($hasTimestamp)
-                $this->timestamp("{$acted}_at")->nullable();
+                if ($hasTimestamp) {
+                    $this->timestamp("{$acted}_at")->nullable();
+                }
 
-            if ($shouldIndex) {
-                if ($hasType)
-                    $this->index(["{$actor}_type", "{$actor}_id"], $indexName);
-                else
-                    $this->index(["{$actor}_id"], $indexName);
+                if ($shouldIndex) {
+                    if ($hasType) {
+                        $this->index(["{$actor}_type", "{$actor}_id"], $indexName);
+                    } else {
+                        $this->index(["{$actor}_id"], $indexName);
+                    }
+                }
+
+                return $field;
             }
-
-            return $field;
-        });
+        );
     }
 
     private function defineCustomMacros(): void
     {
         foreach (config('laravel-actor.custom-macros', []) as $customMacro) {
-            Blueprint::macro(NamingHelper::getActor($customMacro), function ($hasType = true, $hasTimestamp = false, $indexName = null, $shouldIndex = false) use ($customMacro) {
-                return $this->actor($customMacro, $hasType, $hasTimestamp, $indexName, $shouldIndex);
-            });
+            Blueprint::macro(
+                NamingHelper::getActor($customMacro),
+                function ($hasType = true, $hasTimestamp = false, $indexName = null, $shouldIndex = false) use (
+                    $customMacro
+                ) {
+                    return $this->actor($customMacro, $hasType, $hasTimestamp, $indexName, $shouldIndex);
+                }
+            );
         }
     }
 
